e40a7e52d232007e17a22eee5b88cdbee37fd2de
Ivan Nikolsky <enty8080@gmail.com>
2022-06-04 22:42:49 +0200

    Create macho.c

---
+++ macho.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <sys/mman.h>
+
+char payload[1024] = "PAYLOAD:";
+
+int main() {
+    void *ptr = mmap(0, sizeof(payload), PROT_EXEC | PROT_WRITE | PROT_READ, MAP_ANON | MAP_PRIVATE, -1, 0);
+
+    if (ptr == MAP_FAILED)
+        return -1;
+
+    memcpy(ptr, payload, sizeof(payload));
+    int (*sc)() = ptr;
+    sc();
+
+    return 0;
+}

